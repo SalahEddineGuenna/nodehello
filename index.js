48784593368d19171016d8852b0521a83323e3fc
salah eddine <salimali1768@gmail.com>
2022-12-24 18:57:00 +0100

    trigger

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("From Container");
+    return res.send("From Container trigger");
 })
 
 app.get('/api', function (req, res) {
